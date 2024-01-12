74bc85a731fe583c6a5b18945e95040d620d6096
Việt An <vietan00892b@gmail.com>
2024-01-12 10:28:24 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
