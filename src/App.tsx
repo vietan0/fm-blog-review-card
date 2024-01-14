24216a7285cce2b4742c46686c297f37f713123d
Việt An <vietan00892b@gmail.com>
2024-01-14 19:14:28 +0700

    feat: build card layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
+import illus from './assets/images/illustration-article.svg';
+import avatar from './assets/images/image-avatar.webp';
+
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="attribution">
-        Challenge by{' '}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
+    <main>
+      <h1 className="sr-only">
+        Blog preview card - Frontend Mentor Challenge - Solution by Viet An
+      </h1>
+      <div id="card">
+        <img src={illus} alt="" />
+        <div className="main-info">
+          <p className="tag">Learning</p>
+          <p>Published 21 Dec 2023</p>
+          <a className="title" href="/">
+            HTML & CSS foundations
+          </a>
+          <p className="desc">
+            These languages are the backbone of every website, defining structure, content, and
+            presentation.
+          </p>
+        </div>
+        <div className="author">
+          <img src={avatar} alt="" />
+          <p className="name">Greg Hooper</p>
+        </div>
+        <div className="attribution">
+          Challenge by{' '}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{' '}
+          <a href="https://github.com/vietan0" target="_blank">
+            Việt An
+          </a>
+          .
+        </div>
       </div>
-    </>
+    </main>
   );
 }
 
